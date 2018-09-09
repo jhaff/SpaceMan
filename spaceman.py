6e6275a9bbd1d39c3e106bedda1d0ae16b8036a6
jhaff <jacobhaff2@gmail.com>
2018-09-09 12:26:07 -0700

    added getWord function to pull word randomly from a dictionary at freebsd.org - modified program to use said word. Fixed bug so that user now sees word when they lose

--- spaceman.py
+++ spaceman.py
@@ -1,5 +1,8 @@
-import random, sys
 from typing import List
+from random import randint,random
+import random, sys
+import time
+import requests
 
 # Utility functions
 def print_word_to_guess(letters: List) -> None:
@@ -10,21 +13,38 @@ def print_guesses_taken(current: int, total: int) -> None:
     #Print how many chances the player has used
     print("You are on guess {0}/{1}.".format(current, total))
 
+def getword():
+    word_site = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
+
+    response = requests.get(word_site)
+    WORDS = response.content.splitlines()
+
+    word = (WORDS[randint(0,len(WORDS)-1)])
+    word = str(word)
+    print(word[2:-1].lower());
+    wordLength = len(word[2:-1].lower())
+
+    return(word[2:-1].lower())
+
+
 #use of a tuple because words will not change
-wordList = (
-"bowl", "power", "window", "computer", "phone", "juice", "macbook", "desktop",
-"laptop", "dog", "cat", "lemon", "cable", "mirror", "hat", "spoon", "spongebob",
-"squidward", "patrick"
-       )
+# wordList = (
+# "bowl", "power", "window", "computer", "phone", "juice", "macbook", "desktop",
+# "laptop", "dog", "cat", "lemon", "cable", "mirror", "hat", "spoon", "spongebob",
+# "squidward", "patrick"
+#        )
 
 guess_word = []
 # randomly choose single word from the list
-secret_word = random.choice(wordList)
+secret_word = getword();
 #make a variable for the length of the chosen word
-wordLength = len(secret_word)
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 #form empty mutable list to store guessed letters
 letter_storage = []
+
+wordLength = len(secret_word)
+
+
 
 def word_description():
 
@@ -61,14 +81,13 @@ def guessing():
                 print_guesses_taken(guess_taken, MAX_GUESS)
                 if not '_ ' in guess_word:
                     print("You won!")
-                    print("Game Over!")
                     break
             else:
                 print("The letter is not in the word. Try Again!")
                 guess_taken += 1
                 print_guesses_taken(guess_taken, MAX_GUESS)
-                if guess_taken == 10:
-                    print(" Sorry you lost! The secret word was {0}".format(SECRET_WORD))
+                if guess_taken == 7:
+                    print(" Sorry you lost! The secret word was {0}.".format(secret_word))
 
 #main function to call the others
 def spaceman():
