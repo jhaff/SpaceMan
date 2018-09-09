87b734efba52148370b28df785e921de7e896101
jhaff <jacobhaff2@gmail.com>
2018-09-09 11:09:53 -0700

    Constructed main spaceman method to run the other functions. Game is buggy and claims user has won after guessing one correct letter

--- spaceman.py
+++ spaceman.py
@@ -1,21 +1,20 @@
 import random
 
-#form the list of words and select one randomly
-def load_word():
-  #use of a tuple because words will not change
-  wordList = (
-  "bowl", "power", "window", "computer", "phone", "juice", "macbook", "desktop",
-  "laptop", "dog", "cat", "lemon", "cable", "mirror", "hat"
-           )
 
-  guessWord = []
-  # randomly choose single word from the list
-  secret_word = random.choice(wordList)
-  #make a variable for the length of the chosen word
-  wordLength = len(secret_word)
-  alphabet = "abcdefghijklmnopqrstuvwxyz"
-  #form empty mutable list to store guessed letters
-  letter_storage = []
+#use of a tuple because words will not change
+wordList = (
+"bowl", "power", "window", "computer", "phone", "juice", "macbook", "desktop",
+"laptop", "dog", "cat", "lemon", "cable", "mirror", "hat"
+       )
+
+guess_word = []
+# randomly choose single word from the list
+secret_word = random.choice(wordList)
+#make a variable for the length of the chosen word
+wordLength = len(secret_word)
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+#form empty mutable list to store guessed letters
+letter_storage = []
 
 def word_description():
 
@@ -49,7 +48,7 @@ def guessing():
             if guess in secret_word:
                 print("Your guess was correct!")
                 #loop through each character in secret_word and compare it to the guess letter
-                for x in range(0, length_word):
+                for x in range(0, wordLength):
                     if secret_word[x] == guess:
                         guess_word[x] = guess
                     print(guess_word)
@@ -67,15 +66,11 @@ def guessing():
 
 
 
-# # run the game
-# def spaceman():
-#
-#     load_word();
-#     word_description();
-#     guessing();
-    # FILL IN YOUR CODE HERE...
+# run the game
+def spaceman():
+
+    word_description();
+    guessing();
 
 
-#
-# secret_word = load_word()
-# spaceman(load_word())
+spaceman();
