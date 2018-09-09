e08dde3f23000f0433a0f05f0865cff13c39fb5d
jhaff <jacobhaff2@gmail.com>
2018-09-09 11:21:00 -0700

    refactored code to add utility functions to print guesses taken and the word to guess. Refactored guessing function to utilize utility functions. Fixed bug where user would win after guessing one letter. Changed max guesses to 7

--- spaceman.py
+++ spaceman.py
@@ -1,4 +1,16 @@
-import random
+import random, sys
+from typing import List
+
+
+# Utility functions
+def print_word_to_guess(letters: List) -> None:
+    #Print the current word to guess
+    print("Word to guess: {0}".format(" ".join(letters)))
+
+
+def print_guesses_taken(current: int, total: int) -> None:
+    #Print how many chances the player has used
+    print("You are on guess {0}/{1}.".format(current, total))
 
 
 #use of a tuple because words will not change
@@ -26,45 +38,44 @@ def word_description():
 
     print("You may only enter 1 letter from a-z\n\n")
 
-    print(guess_word)
+    print_word_to_guess(guess_word)
 
 
     #user guesses a letters
+
 def guessing():
+
+    #Main game loop to have user guess letters and show results
+
     guess_taken = 1
+    MAX_GUESS = 7
+    print_guesses_taken(guess_taken, MAX_GUESS)
 
-    while guess_taken < 10:
-
-
-        guess = input("Choose a letter\n").lower()
-
+    while guess_taken < MAX_GUESS:
+        guess = input("Pick a letter\n").lower()
         if not guess in alphabet: #check input
-            print("Enter a letter from a-z")
-        elif guess in letter_storage: #check if letter has been already used
+            print("Enter a letter from a-z ALPHABET")
+        elif guess in letter_storage: #checkif letter has been already used
             print("You have already guessed that letter!")
         else:
-
             letter_storage.append(guess)
             if guess in secret_word:
-                print("Your guess was correct!")
-                #loop through each character in secret_word and compare it to the guess letter
-                for x in range(0, wordLength):
-                    if secret_word[x] == guess:
-                        guess_word[x] = guess
-                    print(guess_word)
-
-                #if no dashes left in the guess_word, player has won.
-                if not '-' in guess_word:
+                print("You guessed correctly!")
+                for i in range(0, wordLength):
+                    if secret_word[i] == guess:
+                        guess_word[i] = guess
+                print_word_to_guess(guess_word)
+                print_guesses_taken(guess_taken, MAX_GUESS)
+                if not '_ ' in guess_word:
                     print("You won!")
+                    print("Game Over!")
                     break
             else:
                 print("The letter is not in the word. Try Again!")
                 guess_taken += 1
-                #if the user is out of guesses, the game ends and the secret word is shown
+                print_guesses_taken(guess_taken, MAX_GUESS)
                 if guess_taken == 10:
-                    print("Sorry, You lost! The secret word was",   secret_word);
-
-
+                    print(" Sorry you lost! The secret word was {0}".format(SECRET_WORD))
 
 # run the game
 def spaceman():
